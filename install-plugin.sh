f6c934aa76e625bd6e9dc0f087e8ac6d2e81591b
Jaromír Pufler <jaromir.pufler@gmail.com>
2016-08-29 15:50:56 +0200

    plugin un/installtion restarts redmine

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker-compose exec redmine bundle exec rake redmine:plugins NAME=$1 RAILS_ENV=production
+docker-compose restart redmine
--- uninstall-plugin.sh
+++ uninstall-plugin.sh
@@ -2,3 +2,4 @@
 
 docker-compose exec redmine bundle exec rake redmine:plugins:migrate NAME=$1 VERSION=0 RAILS_ENV=production
 rm -rf plugins/$1
+docker-compose restart redmine
